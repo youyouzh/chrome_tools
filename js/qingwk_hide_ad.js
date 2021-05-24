ea0aeffd43f2f6e463afb10596bfc7c7b8f13713
youyouzh <1406558940@qq.com>
2021-05-25 01:31:49 +0800

    update

--- js/qingwk_hide_ad.js
+++ js/qingwk_hide_ad.js
@@ -1,8 +1,7 @@
 // 轻微课隐藏弹窗广告
 
-
+const popup_ad_elements = document.getElementsByClassName('c-meiqia-wrapper');
 function hide_popup_ads() {
-    const popup_ad_elements = document.getElementsByClassName('c-meiqia-wrapper');
     if (popup_ad_elements && popup_ad_elements.length > 0) {
         popup_ad_elements[0].style.display = 'none';
     }
