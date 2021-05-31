9019be5eb40d110f74a34d506665f1e121552ae3
youyouzh <1406558940@qq.com>
2021-06-01 00:48:43 +0800

    use hide, avoid setInterval

--- js/qingwk_hide_ad.js
+++ js/qingwk_hide_ad.js
@@ -4,7 +4,10 @@ const popup_ad_elements = document.getElementsByClassName('c-meiqia-wrapper');
 function hide_popup_ads() {
     if (popup_ad_elements && popup_ad_elements.length > 0) {
         popup_ad_elements[0].style.display = 'none';
+        popup_ad_elements[0].style.zIndex = '-10';
     }
 }
 
-setInterval(hide_popup_ads, 100);
+hide_popup_ads();
+
+// setInterval(hide_popup_ads, 100);
