7f5f6bffee53151ce3517da4406940e9ece24ad4
uusama <1406558940@qq.com>
2025-03-05 14:29:51 +0800

    add try catch.

--- common/function.js
+++ common/function.js
@@ -29,14 +29,18 @@ _u_api = {
         // Immediately return a promise and start asynchronous work
         return new Promise((resolve, reject) => {
             // Asynchronously fetch all data from storage.sync.
-            chrome.storage.local.get(keys, (items) => {
-                // Pass any observed errors down the promise chain.
-                if (chrome.runtime.lastError) {
-                    return reject(chrome.runtime.lastError);
-                }
-                // Pass the data retrieved from storage down the promise chain.
-                resolve(items[keys]);
-            });
+            try {
+                chrome.storage.local.get(keys, (items) => {
+                    // Pass any observed errors down the promise chain.
+                    if (chrome.runtime.lastError) {
+                        return reject(chrome.runtime.lastError);
+                    }
+                    // Pass the data retrieved from storage down the promise chain.
+                    resolve(items[keys]);
+                });
+            } catch (e) {
+                return reject(e);
+            }
         });
     },
     /**
